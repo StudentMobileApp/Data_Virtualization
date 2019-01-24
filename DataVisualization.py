ddc6db19e89c9249e19362ead30fdc73076176d7
StudentMobileApp <44848378+StudentMobileApp@users.noreply.github.com>
2019-01-23 18:17:45 -0800

    AV Updated files
    Added functionality for example. Use menu to view different types of graphs. This version uses different data for each different type of graph at the moment.

--- DataVisualization.py
+++ DataVisualization.py
@@ -1,14 +1,16 @@
 import tkinter as tk
 import csv
 import matplotlib
+
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.patches import ConnectionPatch
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 
 
 class TopFrame(tk.Frame):
     def __init__(self, master):
-        tk.Frame.__init__(self, bg='white',height=45, pady=3)
+        tk.Frame.__init__(self, bg='white', height=45, pady=3)
         self.master = master
 
         # Create and set all widgets for this frame
@@ -29,7 +31,8 @@ class TopFrame(tk.Frame):
         self.meaningLabel = tk.Label(self, text='Meaning of Weights:')
         self.weightMeaning = tk.StringVar(self)
         self.weightMeaning.set("distance between nodes")  # default value of drop down
-        self.dropDown1 = tk.OptionMenu(self, self.weightMeaning, "distance between nodes", "frequency of communication","frequency of visits")
+        self.dropDown1 = tk.OptionMenu(self, self.weightMeaning, "distance between nodes", "frequency of communication",
+                                       "frequency of visits")
         # layout widgets
         self.meaningLabel.grid(row=0, column=2, padx=(50, 0))
         self.dropDown1.grid(row=0, column=3)
@@ -45,15 +48,19 @@ class TopFrame(tk.Frame):
         self.similarityButton1.grid(row=1, column=4, sticky='w', padx=(60, 0))
         self.similarityButton2.grid(row=2, column=4, sticky='w', padx=(60, 0))
 
+
 class CenterFrame(tk.Frame):
-    def __init__(self, master):
+
+    def __init__(self, master, bottomFrame):
         tk.Frame.__init__(self, bg='gray1', padx=3, pady=3)
         self.master = master
         self.createCenterFrameSections()
+        self.bottomFrame = bottomFrame
 
         # create cpc from Phillipe
-        self.values = [[]]
-        self.API()
+        #self.values = [[]]
+        #self.cpcAPI()
+        #self.spcAPI()
 
     def createCenterFrameSections(self):
         # create the center widgets
@@ -68,13 +75,49 @@ class CenterFrame(tk.Frame):
         self.ctr_mid.grid(row=0, column=1, sticky="nsew")
         self.ctr_right.grid(row=0, column=2, sticky="ns")
 
-    def API(self):
-        self.ReadFile()
+    def cpcAPI(self):
+        for widget in self.ctr_mid.winfo_children():                # Clears widgets on the frame
+            widget.destroy()
+        for widget in self.bottomFrame.winfo_children():                # Clears widgets on the bottom frame
+            widget.destroy()
+        self.values = [[]]
+        self.ReadFile1()
         self.Output()
         self.createCPC()
 
-    def ReadFile(self):  # Reads values into 2d array
+    def cpcAPI2(self):
+        for widget in self.ctr_mid.winfo_children():                # Clears widgets on the frame
+            widget.destroy()
+        for widget in self.bottomFrame.winfo_children():                # Clears widgets on the bottom frame
+            widget.destroy()
+        self.values = [[]]
+        self.ReadFile1()
+        self.Output()
+        self.createSeperateCPC()
+
+    def spcAPI(self):
+        for widget in self.ctr_mid.winfo_children():                # Clears widgets on the frame
+            widget.destroy()
+        for widget in self.bottomFrame.winfo_children():                # Clears widgets on the bottom frame
+            widget.destroy()
+        self.values = [[]]
+        self.ReadFile2()
+        self.Output()
+        self.createSPC()
+
+    def ReadFile1(self):  # Reads values into 2d array
         with open('input.csv', 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            count = 0
+            for line in csv_reader:
+                self.values.append([])
+                line = list(line)
+                for i in line:
+                    self.values[count].append(i)
+                count = count + 1
+
+    def ReadFile2(self):  # Reads values into 2d array
+        with open('input2.csv', 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
             count = 0
             for line in csv_reader:
@@ -93,6 +136,73 @@ class CenterFrame(tk.Frame):
                 print(currentLine, end=" ")
         print(" ")
 
+    def setToolbar(self, graph):
+        toolbar1 = NavigationToolbar2Tk(graph, self.bottomFrame)
+        toolbar1.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+    def createSPC(self):
+        print("")
+        print("The matrix has been converted into x and y coordinates")
+        ogX = []
+        ogY = []
+        first = []
+        second = []
+        xNums = 0
+        yNums = 0
+        totalx = 0
+        totaly = 0
+
+        for i in range(len(self.values)):
+            for j in range(len(self.values[i])):
+                if (int(self.values[i][j]) > 0):
+                    if (xNums <= yNums):  # X-values for graph
+                        totalx = int(self.values[i][j]) + int(totalx)  # Total  distance x
+                        first.append(int(totalx))
+                        ogX.append(int(self.values[i][j]))  # Original x-values (for SPC)
+                        xNums = xNums + 1
+                    else:  # Y-values for graph
+                        totaly = int(self.values[i][j]) + int(totaly)  # Total  distance y
+                        second.append(int(totaly))
+                        ogY.append(int(self.values[i][j]))  # Original y-values (for SPC)
+                        yNums = yNums + 1
+
+                else:
+                    continue
+
+        print("CPC Values")  # Printing coordinate pairs
+        for m in range(len(first)):
+            print("(" + str(first[m]) + "," + str(second[m]) + ")")
+
+        print("\n SPC values")
+        for m in range(len(ogX)):
+            print("(" + str(ogX[m]) + "," + str(ogY[m]) + ")")
+
+        fig = plt.figure()  # Creating the sub plot and plot points
+        p1 = fig.add_subplot(131)
+        p2 = fig.add_subplot(132)
+        p3 = fig.add_subplot(133)
+
+        p1.plot(ogX[0], ogY[0])
+        p2.plot(ogX[1], ogY[1])
+
+        xy = (ogX[0], ogY[0])  # These are for original values to be graphed in SPC
+        ab = (ogX[1], ogY[1])
+        cd = (ogX[2], ogY[2])
+        con = ConnectionPatch(xyA=ab, xyB=xy, coordsA="data", coordsB="data",
+                              axesA=p2, axesB=p1, color="purple")
+        p2.add_artist(con)
+
+        p1.plot(ogX[0], ogY[0], 'ro', markersize=10)  # Plot the SPC points on each graph
+        p2.plot(ogX[1], ogY[1], 'ro', markersize=10)
+        p3.plot(ogX[2], ogY[2], 'ro', markersize=10)
+
+        print("\nPrinting the SPC for the first two coordinate pairs")
+        #plt.show()
+        spc = FigureCanvasTkAgg(fig, self.ctr_mid)  # A tk.DrawingArea.
+        spc.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        self.setToolbar(spc)
+
     def createCPC(self):
         xCoords = []
         yCoords = []
@@ -100,13 +210,13 @@ class CenterFrame(tk.Frame):
         totalY = 0
         currentClass = 1
         xLength = len(self.values)  # Rows
-        
+
         fig = plt.Figure()
         a = fig.add_subplot(111)  # .plot(x, y) # 1 by 1, graph #1?
         a.set_title("Collocated Paired Coordinates")
         a.set_xlabel('x')
         a.set_ylabel('y')
-        
+
         for i in range(1, (xLength - 1)):  # 1- 6
             yLength = len(self.values[i])  # Columns
             for j in range(1, (yLength - 1)):
@@ -133,60 +243,60 @@ class CenterFrame(tk.Frame):
         cpc = FigureCanvasTkAgg(fig, self.ctr_mid)  # A tk.DrawingArea.
         cpc.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
+        self.setToolbar(cpc)
+
+    # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # TASK 5 Part A : Draw All Graphs in CPC separately
+    # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    def drawCPC(self, currentClass, fig, xCoords, yCoords):  # Draws the CPC by resizing the figure and creating the graph
+        n = len(fig.axes)
+        for b in range(n):
+            fig.axes[b].change_geometry(n + 1, 1, b + 1)  # Resizing (row, column, position)
+        ax = fig.add_subplot(n + 1, 1, n + 1)
+        ax.set_xlabel('x')  # Styling the graph
+        ax.set_ylabel('y')
+        ax.set_title("CPC for Class: " + str(currentClass))
+        ax.plot(xCoords, yCoords, '--bo')  # Plotting the graph
+
+    def createSeperateCPC(self):  # This is the "core" method of this task. Calculates CPC values and draws the graph
+        xCoords, yCoords = [], []  # Stores the X and Y coordinates
+        totalX, totalY, currentClass = 0, 0, 1
+        xLength = len(self.values)  # Number of rows
+
+        fig = plt.figure(figsize=(7, 7))  # Sizing the subplots and spacing
+        fig.subplots_adjust(hspace=0.5)
+
+        for i in range(1, (xLength - 1)):  # Exclude IDS from data
+            yLength = len(self.values[i])  # Number of columns
+            for j in range(1, (yLength - 1)):  # Exclude IDs from data
+                if (int(self.values[i][yLength - 1]) == currentClass):  # Process each class
+                    if ((len(xCoords)) <= (len(yCoords))):  # X-values for graph
+                        totalX = float(self.values[i][j]) + float(totalX)
+                        xCoords.append(float(totalX))  # Store the current total X distance
+                    else:  # Y-values for graph
+                        totalY = float(self.values[i][j]) + float(totalY)
+                        yCoords.append(float(totalY))  # Store the current total Y distance
+                else:
+                    self.drawCPC(currentClass, fig, xCoords, yCoords)  # Draw the CPC
+                    xCoords.clear()  # Reset everything for next class
+                    yCoords.clear()
+                    totalX, totalY = 0, 0
+                    currentClass = currentClass + 1
+                    xCoords.append(float(self.values[i][j]))  # Store the first x-value of next class
+
+        self.drawCPC(currentClass, fig, xCoords, yCoords)  # Draws the last CPC
+        #plt.show()  # Displays the figure
+        dividedCPC = FigureCanvasTkAgg(fig, self.ctr_mid)  # A tk.DrawingArea.
+        dividedCPC.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        self.setToolbar(dividedCPC)
+
+
+
 class BottomFrame(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, bg='white', height=45, pady=3)
         self.master = master
-
-class MainMenu(tk.Menu):
-    def __init__(self, master):
-        tk.Menu.__init__(self, master)
-        #self.master = master
-        self.createMenu()
-
-    def createMenu(self):
-        self.menubar = tk.Menu(self)
-        self.filemenu = tk.Menu(self.menubar, tearoff=0)
-        self.filemenu.add_command(label="New", command=self.donothing)
-        self.filemenu.add_command(label="Open", command=self.openFile)
-        self.filemenu.add_command(label="Save", command=self.donothing)
-        self.filemenu.add_command(label="Save as...", command=self.donothing)
-        self.filemenu.add_command(label="Close", command=self.donothing)
-
-        self.filemenu.add_separator()
-
-        self.filemenu.add_command(label="Exit", command=self.quit)
-        self.add_cascade(label="File", menu=self.filemenu)
-        self.editmenu = tk.Menu(self.menubar, tearoff=0)
-        self.editmenu.add_command(label="Undo", command=self.donothing)
-
-        self.editmenu.add_separator()
-
-        self.editmenu.add_command(label="Cut", command=self.donothing)
-        self.editmenu.add_command(label="Copy", command=self.donothing)
-        self.editmenu.add_command(label="Paste", command=self.donothing)
-        self.editmenu.add_command(label="Delete", command=self.donothing)
-        self.editmenu.add_command(label="Select All", command=self.donothing)
-
-        self.add_cascade(label="Edit", menu=self.editmenu)
-        self.helpmenu = tk.Menu(self.menubar, tearoff=0)
-        self.helpmenu.add_command(label="Help Index", command=self.donothing)
-        self.helpmenu.add_command(label="About...", command=self.donothing)
-        self.add_cascade(label="Help", menu=self.helpmenu)
-
-        #self.config(menu= self.menubar)
-
-
-    def openFile(self):
-        from tkinter.filedialog import askopenfilename
-        self.file = askopenfilename()
-        print(self.file)
-        # showGraphs(file)
-
-    def donothing(self):
-        self.filewin = tk.Toplevel(root)
-        button = tk.Button(self.filewin, text="Do nothing button")
-        button.pack()
 
 
 class MainApplication(tk.Frame):
@@ -197,25 +307,70 @@ class MainApplication(tk.Frame):
         root.grid_rowconfigure(1, weight=1)
         root.grid_columnconfigure(0, weight=1)
 
-        tk.Frame.__init__(self, master, *args, **kwargs) # define settings upon initialization
-        self.master = master # reference to the master widget, the tk window
+        tk.Frame.__init__(self, master, *args, **kwargs)  # define settings upon initialization
+        self.master = master  # reference to the master widget, the tk window
 
         # Call classes to create frames
         self.topFrame = TopFrame(self)
-        self.centerFrame = CenterFrame(self)
         self.bottomFrame = BottomFrame(self)
-        self.mainMenu = MainMenu(self)
+        self.centerFrame = CenterFrame(self, self.bottomFrame)
 
-        #root.grid_rowconfigure(1, weight=1)
-        #root.grid_columnconfigure(0, weight=1)
+
+        tk.Menu.__init__(self, master)
+        self.createMenu()
+
+        # root.grid_rowconfigure(1, weight=1)
+        # root.grid_columnconfigure(0, weight=1)
 
         # Set frames in position
         self.topFrame.grid(row=0, sticky="ew")
         self.centerFrame.grid(row=1, sticky="nsew")
         self.bottomFrame.grid(row=2, sticky="ew")
-        root.config(menu=self.mainMenu)
+        root.config(menu=self.menubar)
+
+    def createMenu(self):
+        self.menubar = tk.Menu(self)
+
+        self.filemenu = tk.Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label="New")
+        self.filemenu.add_command(label="Open")
+        self.filemenu.add_command(label="Save")
+        self.filemenu.add_command(label="Save as...")
+        self.filemenu.add_command(label="Close")
+        self.filemenu.add_separator() # adds a line ---
+        self.filemenu.add_command(label="Exit", command=self.quit)
+        self.menubar.add_cascade(label="File", menu=self.filemenu) # add the FILE menu
+
+        self.viewmenu = tk.Menu(self.menubar, tearoff=0)
+        self.viewmenu.add_command(label="Collocated Paired Coordinates (CPC)", command=self.centerFrame.cpcAPI)
+        self.viewmenu.add_command(label="Seperated CPC(s)", command=self.centerFrame.cpcAPI2)
+        self.viewmenu.add_separator()  # adds a line ---
+        self.viewmenu.add_command(label="Shifted Paired Coordinates (SPC)", command=self.centerFrame.spcAPI)
+        self.menubar.add_cascade(label="View", menu=self.viewmenu) # add the VIEW menu
+
+        self.editmenu = tk.Menu(self.menubar, tearoff=0)
+        self.editmenu.add_command(label="Undo")
+        self.editmenu.add_separator() # adds a line ---
+        self.editmenu.add_command(label="Cut")
+        self.editmenu.add_command(label="Copy")
+        self.editmenu.add_command(label="Paste")
+        self.editmenu.add_command(label="Delete")
+        self.editmenu.add_command(label="Select All")
+        self.menubar.add_cascade(label="Edit", menu=self.editmenu) # add the EDIT menu
+
+        self.helpmenu = tk.Menu(self.menubar, tearoff=0)
+        self.helpmenu.add_command(label="Help Index")
+        self.helpmenu.add_command(label="About...")
+        self.menubar.add_cascade(label="Help", menu=self.helpmenu) # add the HELP menu
+
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     MainApplication(root)
+    while True:  # Scrolling on trackpad (Mac) throws error
+        try:
+            root.mainloop()
+            break
+        except UnicodeDecodeError:
+            pass
