bc3dca090e9ea8f55451e49c78a7080aa6c47c66
StudentMobileApp <44848378+StudentMobileApp@users.noreply.github.com>
2019-01-28 18:52:20 -0800

    Update Project_5A_CPC.py

--- Project_5A_CPC.py
+++ Project_5A_CPC.py
@@ -1,11 +1,12 @@
 # Phillipe Shin
 # 01/21/2019
 # SMART Data Visualization
-#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-import csv                                                                                 # This section sets up the program and reads the file into values[][]
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+import csv  # This section sets up the program and reads the file into values[][]
 import sys
 import math
-import pandas as pd
+import matplotlib
+matplotlib.use("TkAgg")
 from matplotlib import pyplot as plt
 from matplotlib.patches import ConnectionPatch
 import numpy as np
@@ -13,66 +14,61 @@ import numpy as np
 class project(object):
     def API(self, values):
         self.ReadFile(values)
-        self.createCPC(values)
-        
-    def ReadFile(self, values):                                                      
+        self.createCPCinOne(values)
+
+    def ReadFile(self, values):
         with open('input.csv', 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
             count = 0
             for line in csv_reader:
                 values.append([])
-                line=list(line)
+                line = list(line)
                 for i in line:
                     values[count].append(i)
                 count = count + 1
-                
-#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------               
-                                                                                                # TASK 5 Part A
-#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    def drawCPC(self, currentClass, fig, xCoords, yCoords):                  # Draws the CPC by resizing the figure and creating the graph
-        n = len(fig.axes)
-        for b in range(n):
-            fig.axes[b].change_geometry(n+1, 1, b+1)                                 # Resizing (row, column, position)
-        ax = fig.add_subplot(n+1, 1, n+1)                                     
 
-        ax.set_xlabel('x')                                                                             # Styling the graph
-        ax.set_ylabel('y')
-        ax.set_title("CPC for Class: " + str(currentClass))
-        ax.plot(xCoords, yCoords,'--bo')                                                     # Plotting the graph
-        
-    def createCPC(self,values):                                                                # This is the "core" method of this task. Calculates CPC values and draws the graph
-        xCoords, yCoords = [], []                                                                 # Stores the X and Y coordinates
+    # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # TASK 5 Part A : Draw All Graphs in CPC separately
+    # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    def createCPCinOne(self, values):                                                   # This is the "core" method of this task. Calculates CPC values and draws the graph
+        xCoords, yCoords = [], []                                                       # Stores the X and Y coordinates
         totalX, totalY, currentClass = 0, 0, 1
-        xLength = len(values)                                                                     # Number of rows
+        xLength = len(values)                                                           # Number of rows
 
-        fig = plt.figure(figsize=(7,7))                                                            # Sizing the subplots and spacing
-        fig.subplots_adjust(hspace=0.5) 
-        
-        for i in range(1, (xLength - 1)):                                                       # Exclude IDS from data
-            yLength = len(values[i])                                                               # Number of columns
-            for j in range(1, (yLength - 1)):                                                   # Exclude IDs from data 
-                if (int(values[i][yLength - 1]) == currentClass):                        # Process each class
-                    if ((len(xCoords)) <= (len(yCoords))):                                 # X-values for graph
-                        totalX = float(values[i][j]) + float(totalX)                      
-                        xCoords.append(float(totalX))                                         # Store the current total X distance
-                    else:                                                                                    # Y-values for graph
-                        totalY = float(values[i][j]) + float(totalY)    
-                        yCoords.append(float(totalY))                                         # Store the current total Y distance
-                else:                 
-                    self.drawCPC(currentClass, fig, xCoords, yCoords)           # Draw the CPC
-                    xCoords.clear()                                                                    # Reset everything for next class
+        fig = plt.figure(figsize=(7, 7))                                                # Sizing the subplots and spacing
+        fig.subplots_adjust(hspace=0.5)
+        plt.title("Collocated Paired Coordinates")
+        plt.xlabel("X")
+        plt.ylabel("Y")
+
+        for i in range(1, (xLength - 1)):                                               # Exclude IDS from data
+            yLength = len(values[i])                                                    # Number of columns
+            for j in range(1, (yLength - 1)):                                           # Exclude IDs from data
+                if (int(values[i][yLength - 1]) == currentClass):                       # Process each class
+                    if ((len(xCoords)) <= (len(yCoords))):                              # X-values for graph
+                        totalX = float(values[i][j]) + float(totalX)
+                        xCoords.append(float(totalX))                                   # Store the current total X distance
+                    else:                                                               # Y-values for graph
+                        totalY = float(values[i][j]) + float(totalY)
+                        yCoords.append(float(totalY))                                   # Store the current total Y distance
+                else:
+                    plt.plot(xCoords, yCoords, '--o', label="Class: " + str(currentClass))
+                    xCoords.clear()                                                     # Reset everything for next class
                     yCoords.clear()
                     totalX, totalY = 0, 0
-                    currentClass = currentClass +1
-                    xCoords.append(float(values[i][j]))                                      # Store the first x-value of next class
-        
-        self.drawCPC(currentClass, fig, xCoords, yCoords)                       # Draws the last CPC 
-        plt.show()                                                                                        # Displays the figure
-#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------                   
+                    currentClass = currentClass + 1
+                    xCoords.append(float(values[i][j]))                                 # Store the first x-value of next class
+                    totalX = float(values[i][j])
+        plt.plot(xCoords, yCoords, '--o', label="Class: " + str(currentClass))
+        plt.legend()
+        plt.show()                                                                      # Displays the figure
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def main():
-        values = [[]]
-        obj = project()
-        obj.API(values)
+    values = [[]]
+    obj = project()
+    obj.API(values)
+
 
 if __name__ == '__main__':
     main()
